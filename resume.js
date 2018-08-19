45c5daca460441ed238f5f57a5c7b25eb651a155
lbarret4 <llewellynbar4@gmail.com>
2018-08-19 18:29:20 -0400

    Created varibles to be passed into displayHeading function

--- resume.js
+++ resume.js
@@ -1,5 +1,18 @@
+let myName;
+let myCareer;
+let myfield;
+let myBio;
 
 
+displayHeading(myName,myCareer,myfield,myBio);
+console.log('My Interests:');
+displayInterest();
+console.log('My Previous Experience:');
+displayPosition();
+console.log('My Skills:')
+displaySkill()
+
+//Creates the Heading of the resume that inclues name,career field, and personal statement 
 function displayHeading(name,career,field,description){
     console.log(`Name: ${name.toUpperCase()} \n Career: ${career} / ${field} \n Description: ${description}`);
 
