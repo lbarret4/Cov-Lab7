cf206a40b499695649d66b3c099da2b19c136b32
lbarret4 <llewellynbar4@gmail.com>
2018-08-19 20:24:38 -0400

    Update display position function and added comments

--- resume.js
+++ resume.js
@@ -12,18 +12,25 @@ displayPosition();
 console.log('My Skills:')
 displaySkill()
 
-//Creates the heading of the resume that inclues name,career field, and personal statement 
+// Displays the heading of the resume that inclues name,career field, and personal statement 
 function displayHeading(name,career,field,description){
     console.log(`Name: ${name.toUpperCase()} \nCareer: ${career} / ${field} \nDescription: ${description}`);
 
 }
+
+// Displays interest
 function displayInterest(interest){
     console.log(`* ${interest}`);
 }
-function displayPosition(){
+
+// Displays previous experience using input parameters (job title,company name, job description)
+function displayPosition(jobTitle,companyName,description){
+    console.log(`* ${jobTitle} at ${companyName} - ${description}`);
 
 }
 
+
+// Display (un)cool skill to console
 function displaySkill(skill,isCoolSkill){
     console.log(`* ${ (isCoolSkill) ? 'BAM: '+skill : skill}`);
 
